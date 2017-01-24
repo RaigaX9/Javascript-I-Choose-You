0da4983cd82d582b8d1e63b28bdffaa1e9d10193
Nishad Shah <nishad3245@gmail.com>
2017-01-24 16:58:35 -0500

    First commit of javascript code which is the basic stack implementation.

---
+++ codes/basicstack.js
@@ -0,0 +1,20 @@
+var stack=new Array();
+stack.push("A");
+stack.push("B");
+stack.push("C");
+console.log(stack);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+
+console.log("lllllll");
+
+var Q=new Array();
+Q.unshift("A");
+Q.unshift("B");
+Q.unshift("C");
+
+console.log(Q);
+console.log(Q.pop());
+console.log(Q.pop());
+console.log(Q.pop());
